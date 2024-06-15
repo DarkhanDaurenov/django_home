85e92397c9cbdf3d28cd07f644ddb0fea32a8397
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-06-15 15:50:48 +0500

    add catalog/admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 @admin.register(Category)
@@ -13,3 +13,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'product_name', 'product_price', 'product_category')
     list_filter = ('product_category',)
     search_fields = ('product_name', 'product_description')
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number_version', 'product')
